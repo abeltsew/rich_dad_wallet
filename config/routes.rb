b3abc67fd448be407e6c6f9c2d3dcb56443da11d
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-08-03 21:55:12 +0300

    Fix Route Protection

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  # Protect all routes with a login
-  authenticated :user do
-    resources :balances, only: %i[index show new create] do
-      resources :items, only: %i[new create]
-    end
+  resources :balances, only: %i[index show new create] do
+    resources :items, only: %i[new create]
   end
 
   root 'balances#index'
+
+  # Protect all routes with a login
+  # authenticated :user do
+  #   resources :balances, only: %i[index show new create] do
+  #     resources :items, only: %i[new create]
+  #   end
+  # end
 
   get '/home', to: 'home#index'
 end
