4c116501aa5403f6ed4e29ccbc898af5e98dd2e0
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-09-25 20:17:58 +0300

    cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   has_many :items, dependent: :destroy, foreign_key: :author_id
   has_many :balances, dependent: :destroy, foreign_key: :author_id
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
